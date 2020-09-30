15dc9643911763c473d7a805956c51cde5dc6ca2
Henry Doan <henrydoan96@gmail.com>
2020-09-29 20:26:47 -0600

    post crud done

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Modal, Button } from 'semantic-ui-react';
+import PostForm from './PostForm';
 
 class Post extends Component {
+  state = { editing: false }
+
+  open = () => this.setState({ editing: true })
+  close = () => this.setState({ editing: false })
 
   render() {
+    const { id, title, body, updatePost, deletePost } = this.props
+    const { editing } = this.state
     return(
       <>
-        <Header>Post</Header>
+        <Header>{title}</Header>
+        <p>{body}</p>
+        <Modal
+          onClose={() => this.close()}
+          onOpen={() => this.open()}
+          open={editing}
+          trigger={<Button color='yellow' onClick={() => this.open()}>Edit</Button>}
+        >
+          <Modal.Header>Edit Post</Modal.Header>
+          <Modal.Content>
+            <PostForm 
+              id={id}
+              title={title}
+              body={body}
+              updatePost={updatePost}
+              close={this.close}
+            />
+          </Modal.Content>
+        </Modal>
+        <Button color='red' onClick={() => deletePost(id)}>Delete</Button>
       </>
     )
   }
